7415e1538fa7fb5f33fae8891cb210de97b8d81e
Helmut Januschka <helmut@januschka.com>
2005-09-02 02:19:00 +0000

    add_service added

--- bartlby_action.php
+++ bartlby_action.php
@@ -25,6 +25,22 @@ $layout->Tr(
 
 
 switch($_GET[action]) {
+	case 'add_service': 
+		if($_GET[service_server] && $_GET[service_type] &&  $_GET[service_name] &&  $_GET[service_time_from] &&  $_GET[service_time_to] && $_GET[service_interval]) {
+						//&bartlby_config, &server_id, &plugin,&service_name,&plugin_arguments,&notify_enabled,&hour_from,&hour_to,
+						//&min_from,
+						//&min_to,&check_interva	l, &service_type,&service_var,&service_passive_timeout
+			$msg ="Server: " . $_GET[service_server];
+			$ads=bartlby_add_service($btl->CFG, $_GET[service_server], $_GET[service_plugin],$_GET[service_name],$_GET[service_args],1, substr($_GET[service_time_from], 0, 2), substr($_GET[service_time_to], 0, 2), substr($_GET[service_time_from], 3, 2), substr($_GET[service_time_to], 3, 2),$_GET[service_interval],$_GET[service_type],$_GET[service_var], $_GET[service_passive_timeout]);
+			echo "<pre>";
+			var_dump($_GET);
+			$msg .= "Added ($ads)<br>";
+			echo "<script>parent.l.document.location.href='nav.php?r=1'</script>";
+		} else {
+			$msg = "Missing Parameter";	
+		}
+	break;
+	
 	case 'delete_server':
 		if($_GET[server_id]) {
 			$s = bartlby_delete_server($btl->CFG, $_GET[server_id]);
