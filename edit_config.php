86f3787dd703e0e9daf455eaf1e618581e388aa5
Helmut Januschka <helmut@januschka.com>
2005-11-27 02:05:55 +0000

    missing file

---
+++ edit_config.php
@@ -0,0 +1,42 @@
+<?
+include "layout.class.php";
+include "config.php";
+include "bartlby-ui.class.php";
+$btl=new BartlbyUi($Bartlby_CONF);
+
+$layout= new Layout();
+
+$layout->setTitle("Edit Config");
+$layout->Form("fm1", "bartlby_action.php","POST");
+$layout->Table("100%");
+
+$cur_conf=implode(file($btl->CFG), "");
+
+$layout->Tr(
+	$layout->Td(
+			Array(
+				0=>"Server:",
+				1=>$layout->TextArea("cfg_file", $cur_conf, 35, 80) . $layout->Field("action", "hidden", "edit_cfg")
+			)
+		)
+
+);
+
+$layout->Tr(
+	$layout->Td(
+			Array(
+				0=>Array(
+					'colspan'=> 2,
+					"align"=>"right",
+					'show'=>$layout->Field("Subm", "submit", "next->")
+					)
+			)
+		)
+
+);
+
+
+$layout->TableEnd();
+
+$layout->FormEnd();
+$layout->display();
