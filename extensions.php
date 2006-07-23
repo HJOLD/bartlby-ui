e74e701bb11c4d92220d5034debf350faa389e71
Helmut Januschka <helmut@januschka.com>
2006-07-23 02:09:46 +0000

    *** empty log message ***

--- extensions.php
+++ extensions.php
@@ -13,12 +13,26 @@ $btl=new BartlbyUi($Bartlby_CONF);
 
 $layout= new Layout();
 $layout->set_menu("core");
-$layout->setTitle("Installed Extensions");
+$layout->setTitle("Core Extensions");
 $layout->Table("100%");
 
+$core_extensions="";
+$cnt = (int)bartlby_config($btl->CFG, "extension_count");
+
+if($cnt) {
+	for($x=1; $x<=$cnt; $x++) {
+		$path = bartlby_config($btl->CFG, "extension[" . $x . "]");
+		$core_extensions .= "<li>" . basename($path) . "<br>";
+	}
+}
 
 	
-	
+$layout->Tr(
+		$layout->Td(
+				array(0=>$core_extensions)
+			)
+
+		);		
 
 
 $r=$btl->getExtensionsReturn("_About", $layout, true);
