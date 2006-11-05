4e2a55065ba9c2dd8050cc81d536711b1838efc8
Helmut Januschka <helmut@januschka.com>
2006-11-05 20:33:40 +0000

    auto commit

--- extensions/AutoDiscoverAddons/AutoDiscoverAddons.class.php
+++ extensions/AutoDiscoverAddons/AutoDiscoverAddons.class.php
@@ -39,10 +39,45 @@ class AutoDiscoverAddons {
         }
         */
         function _globExt($svcid, $path) {
-        		
+        	  global $defaults;
+        	  $x = 0;
+        	  
+        	  $r = "<script language='JavaScript'>
+        	  	
+        	  	function updatePerfhandlerExt(cnt) {
+        	  		as = document.getElementById('AutoDiscoverAddonsHide');
+        	  		if(as.style.display == 'block') {
+        	  			return;
+        	  		}
+ 				if (searchReq.readyState == 4 || searchReq.readyState == 0) {
+					searchReq.open('GET', 'bartlby_action.php?service_id=" . $defaults[service_id] . "&server_id=" . $defaults[server_id] . "&action=perfhandler_graph', true);
+					searchReq.onreadystatechange = redrawExt; 
+					searchReq.send(null);
+					
+					as.style.display = 'block';
+					
+				}		
+        	  	}
+        	  	function redrawExt(cnt) {
+        	  		st = new Date();
+        	  		if (searchReq.readyState == 4) {
+        	  			if(maxExt == 0) document.location.reload();
+        	  			for(x=0; x<maxExt; x++) {
+        	  				el = document.getElementById('perfh' + x);
+        	  				el.src=el.src + '?a=1&a=' + st;
+        	  				//alert('updated: ' + x);
+        	  			}
+        	  			as = document.getElementById('AutoDiscoverAddonsHide');
+					as.style.display = 'none';
+        	  		}
+        	  	}
+        	  </script>";
+        	  
                 foreach(glob($path . "/" . $svcid . "_*.png") as $fn) {
-                        $r .= "<img src='rrd/" . basename($fn) . "'><br>";
+                        $r .= "<img onClick='updatePerfhandlerExt();' id='perfh" . $x . "' src='rrd/" . basename($fn) . "'><br>";
+                        $x++;
                 }
+                $r = "<div id=AutoDiscoverAddonsHide style='display:none'><font color='red'><img src='extensions/AutoDiscoverAddons/ajax-loader.gif'> reload in progress....</font></div><script>var maxExt = " . $x . ";</script><a href='javascript:updatePerfhandlerExt();'>Update Perfhandler data</A><br>" . $r;
                 return $r;
         }
 
